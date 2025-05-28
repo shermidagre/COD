f5c7b20971017c27dab20e8011e0f6134b46174b
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-05-28 13:48:35 +0200

    Comienzo de examen

--- src/Coche.java
+++ src/Coche.java
@@ -3,9 +3,11 @@ public class Coche {
     String modelo;
     Integer velocidad;
 
+
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
+
     }
 }
