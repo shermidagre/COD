3fb45c070808b2e758db50fe8f24d606f4a40022
Samuel Hermida <Shermidagregores@danielcastelao.org>
2025-05-28 13:49:01 +0200

    creacion gasolina

--- src/Coche.java
+++ src/Coche.java
@@ -2,12 +2,12 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
-
+    Integer gasolina;
 
     public Coche(String modelo, String matricula) {
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
-
+        this.gasolina = 0;
     }
 }
